22dc944291418726b89d1d567f61faa9d6bc5f52
Sojimon <sojimon.cy@titechnologies.org>
2017-08-16 18:35:23 +0530

    new actStub.js file

---
+++ actStub.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const Sinon = require('sinon')
+
+/**
+ *
+ *
+ * @class ActStub
+ */
+class ActStub {
+  /**
+   * Creates an instance of ActStub.
+   *
+   * @memberOf ActStub
+   */
+  constructor (hemera) {
+    this.s = null
+    this.hemera = hemera
+  }
+
+  /**
+   *
+   *
+   * @param {any} pattern
+   * @param {any} error
+   * @param {any} args
+   * @returns
+   *
+   * @memberOf ActStub
+   */
+  stub (pattern, error, args) {
+	//  console.log('========> inside act Stub <==========')
+    if (!this.s) {
+      this.s = Sinon.stub(this.hemera, 'act')
+    }
+    this.s.withArgs(pattern).callsFake((pattern, cb) => {
+      // respect act calls without a callback
+      if (cb) {
+        return cb.call(this.hemera, error, args)
+      }
+    })
+
+    this.s.callThrough()
+
+    return this.s
+  }
+
+  /**
+   *
+   *
+   *
+   * @memberOf ActStub
+   */
+  restore () {
+    if (this.s) {
+      this.s.restore()
+    }
+  }
+}
+
+module.exports = ActStub
